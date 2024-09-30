b4abbcc1cc50bf820b010bc713d69555531910f9
Archana <a.rchana0486@gmail.com>
2024-09-30 13:17:44 +0530

    hompage added

---
+++ src/main/java/objectRepository/Homepage.java
@@ -0,0 +1,8 @@
+package objectRepository;
+
+public class Homepage {
+	public static void main(String[] args) {
+		
+	}
+
+}
--- src/test/java/sample/Hello.java
+++ src/test/java/sample/Hello.java
@@ -3,6 +3,8 @@ package sample;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
 		
 	}
 
